7c4b9ff5df1203a364dc818c3fe5409f0d477a17
NTGAli <alintg@outlook.com>
2024-04-10 07:42:50 +0330

    Button

--- app/src/main/java/com/ntg/budgetapp/MainActivity.kt
+++ app/src/main/java/com/ntg/budgetapp/MainActivity.kt
@@ -3,14 +3,24 @@ package com.ntg.budgetapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ntg.budgetapp.ui.theme.BudgetAppTheme
+import com.ntg.components.Button
+import com.ntg.components.ButtonSize
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +32,40 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    var click by remember {
+                        mutableStateOf(false)
+                    }
+                    Column {
+                        Button(
+                            modifier = Modifier.fillMaxWidth().padding(top = 16.dp),
+                            text = "Test button", loading = click, size = ButtonSize.XL){
+                            click = !click
+                        }
+
+                        Button(
+                            modifier = Modifier.fillMaxWidth().padding(top = 16.dp),
+                            text = "Test button", loading = click, size = ButtonSize.LG){
+                            click = !click
+                        }
+
+
+                        Button(
+                            modifier = Modifier.fillMaxWidth().padding(top = 16.dp),
+                            text = "Test button", loading = click, size = ButtonSize.MD){
+                            click = !click
+                        }
+
+
+                        Button(
+                            modifier = Modifier.fillMaxWidth().padding(top = 16.dp),
+                            text = "Test button", loading = click, size = ButtonSize.XS){
+                            click = !click
+                        }
+
+
+
+                    }
                 }
             }
         }
